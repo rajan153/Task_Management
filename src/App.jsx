549e3258058218d435ca852522699ff9055f4cfb
Rajan <rajanmasih154@gmail.com>
2025-01-22 12:47:24 +0530

    Setup the frontend for project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TaskManagementSystem from "./components/TaskManagementSystem";
+
+function App() {
+  return (
+    <div>
+      <TaskManagementSystem />
+    </div>
+  );
+}
+
+export default App;
