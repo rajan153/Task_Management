3d27f134c3a8d2d36f07e13493a26bc5a602565a
Rajan <rajanmasih154@gmail.com>
2025-01-27 01:23:35 +0530

    Added: Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import TaskManagementSystem from "./components/TaskManagementSystem";
+import TaskManagementSystem from "./pages/TaskManagementSystem";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import Login from "./components/Auth/Login";
+import Signup from "./components/Auth/Signup";
+import Home from "./pages/Home";
+import SocialMediaSystem from "./pages/SocialMediaSystem";
 
 function App() {
   return (
     <div>
-      <TaskManagementSystem />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/task-management" element={<TaskManagementSystem />} />
+          <Route path="/social-media" element={<SocialMediaSystem />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
